2a621bce1434757772671f4479d86f5f27066617
zhangfanlong <zhangfanlong@yahoo.cn>
2011-12-20 10:16:15 +0800

    memory mach_self

--- memory/README.txt
+++ memory/README.txt
@@ -2,3 +2,4 @@
 （1）External_Memory_Manager是使用外部内存管理的接口。
 （2）Virtual_Memory_Interface是使用虚拟内存接口。
 （3）Netname_and_NetMeory是使用Netname和Netmempry的例子，不可以使用咯。
+（4）mach_self.c是获得基本信息的代码。
---
+++ memory/mach_self.c
@@ -0,0 +1,27 @@
+/*获得一些基本信息：
+使用mach_task_self()获得task.
+使用mach_host_self()获得host.
+
+*/
+#include <stdio.h>
+#include <mach.h>
+
+/*测试结果:
+task=//未测试；
+host=23;
+*/
+int main()
+{
+	host_t host;
+	task_t task;
+	
+	kern_return_t kr;
+	
+	task=mach_task_self();
+	printf("task is %d\n",task);
+	host=mach_host_self();
+	printf("host is %d\n",host);
+	
+	return 0;
+}
+
