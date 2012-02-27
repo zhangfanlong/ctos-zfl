fa9b91b25bea42e7730274e6312a6b01aa635b42
zhangfanlong <zhangfanlong@yahoo.cn>
2012-02-27 08:42:29 +0800

    the new beginning

--- memory/External_Memory_Manager/gun_external_memory_op.c
+++ memory/External_Memory_Manager/gun_external_memory_op.c
@@ -43,7 +43,7 @@ kern_return_t memory_object_destroy(memory_object_control_t memory_control,
 									kern_return_t reason);
 
 //6.4 Memory Objects and Data
-//memory_object_data_return：Writes data back to a memory object
+//memory_object_data_return()：Writes data back to a memory object
 //写数据,将内存中data的数据写入到object中的offset中
 kern_return_t memory_object_data_return(memory_object_t memory_object,
 										memory_object_control_t memory_control,
@@ -53,16 +53,19 @@ kern_return_t memory_object_data_return(memory_object_t memory_object,
 										boolean_t dirty,
 										boolean_t kernel_copy);
 
-//memory_object_data_request:Requests data from a memory object
-//使数据可以访问：调入到内存中，使得可以访问。
+//memory_object_data_request():Requests data from a memory object
+//访问数据。
 kern_return_t memory_object_data_request(memory_object_t memory_object,
 									     memory_object_control_t memory_control,
 										 vm_offset_t offset,
 										 vm_offset_t length,
 										 vm_prot_t desired_access);
 
-//memory_object_data_supply：Supplies data for a region of a memory object
-//不明白啊
+//memory_object_data_supply()：Supplies data for a region of a memory object
+/*
+同memory_object_data_request（）相似，具体？？
+提供给内核数据（指定内存对象）：
+*/
 kern_return_t memory_object_data_supply (memory_object_control_t memory_control, 
 										 vm_offset_t offset,
 										 vm_offset_t data,
@@ -75,6 +78,7 @@ kern_return_t memory_object_data_supply (memory_object_control_t memory_control,
 The function memory_object_supply_completed indicates that 
 a previous memory_ object_data_supply has been completed.
 */
+//supply 是否完成。
 kern_return_t memory_object_supply_completed(memory_object_t memory_object,
 											 memory_object_control t memory_control,
 											 vm_offset_t offset,
@@ -82,27 +86,32 @@ kern_return_t memory_object_supply_completed(memory_object_t memory_object,
 											 kern_return_t result,
 											 vm_offset_t error_offset);
 
+//memory_object_data_error()
 /*
 The function memory_object_data_error indicates that the memory manager cannot 
 return the data requested for the given region, specifying a reason for the error. 
 This is typically used when a hardware error is encountered.
 */
+//数据错误
 kern_return_t memory_object_data_error (memory_object_control_t memory_control,
 										vm_offset_t offset,
 										vm_size_t size,
 										kern_return_t reason);
 
+//memory_object_data_unavailable()
 /*
 The function memory_object_data_unavailable indicates that 
 the memory object does not have data for the given region and
 that the kernel should provide the data for this range. 
 */
+//数据不可用
 kern_return_t memory_object_data_unavailable(memory_object_control_t memory_control,
 											 vm_offset_t offset,
 											 vm_size_t size,
 											 kern_return_t reason);
 
-//
+//memory_object_copy()
+//copy已有的内存对象
 kern_return_t memory_object_copy (memory_object_t old_memory_object,
 								  memory_object_control_t old_memory_control,
 								  vm_offset_t offset,
@@ -110,7 +119,7 @@ kern_return_t memory_object_copy (memory_object_t old_memory_object,
 								  memory_object_t new_memory_object);
 
 /*
-不使用的接口
+已经不在使用的接口，老的接口
 */
 memory_object_data_write();
 memory_object_data_provided();
@@ -125,9 +134,17 @@ kern_return_t memory_object_lock_request(memory_object_control_t memory_control,
 										 boolean_t should_flush,
 										 vm_prot_t lock_value,
 										 mach_port_t reply_to);
+kern_return_t memory_object_lock_completed(memory_object_t memory_object,
+										   memory_object_control_t memory_control,
+										   vm_offset_t offset,
+										   vm_size_t length);
+kern_return_t memory_object_data_unlock(memory_object_t memory_object,
+									    memory_object_control_t memory_control,
+										vm_offset_t offset,
+										vm_size_t length,
+										vm_prot_t desired_access);
 
 //6.6 Memory Object Attributes
-
 //The memory_object_get_attributes function retrieves the current attributes for the specified memory object.
 kern_return_t memory_object_get_attributes(memory_object_control_t memory_control,
 										   boolean_t *object_ready,
